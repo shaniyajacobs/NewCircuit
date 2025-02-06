a952a92181a559ff5efea4871e56a60825e54c2a
Kaavya Mahendran <kaavya.mahendran@gmail.com>
2025-02-06 14:12:33 -0800

    centered divs

--- src/components/ClientTestimonials.js
+++ src/components/ClientTestimonials.js
@@ -6,11 +6,10 @@ const ClientTestimonials = () => {
       <div className="flex gap-5 max-md:flex-col">
         <div className="w-[69%] max-md:ml-0 max-md:w-full" data-name="column">
           <div className="flex flex-col items-end w-full max-md:mt-10 max-md:max-w-full">
-            <div
-              className="text-4xl font-bold text-center text-white max-md:max-w-full"
-              data-name="What Our Clients Say About Us"
-            >
-              What Our Clients Say About Us
+            <div className="w-full max-w-[948px] mx-auto text-right">
+              <h2 className="text-4xl font-bold text-white">
+                What Our Clients Say About Us
+              </h2>
             </div>
             <div className="self-stretch mt-6 mr-9 max-md:mr-2.5 max-md:max-w-full">
               <div className="flex gap-5 max-md:flex-col">
@@ -32,7 +31,7 @@ const ClientTestimonials = () => {
                         />
                       </div>
                       <div
-                        className="ml-5 w-[65%] max-md:ml-0 max-md:w-full"
+                        className="ml-4 w-[65%] max-md:ml-0 max-md:w-full"
                         data-name="column"
                       >
                         <div className="flex flex-col grow px-9 pt-9 pb-5 mt-14 w-full bg-white rounded-[30px] text-zinc-800 max-md:px-5 max-md:mt-10">
@@ -45,22 +44,20 @@ const ClientTestimonials = () => {
                             maecenas. Suspendisse sed magna eget nibh in turpis.
                             Consequat duis diam lacus arcu
                           </div>
-                          <div className="self-start mt-3 text-2xl text-center max-md:ml-1">
-                            -
+                          <div className="self-start mt-3 text-lg text-center max-md:ml-1">
+                            - Tom, 24 years old
                           </div>
-                          <div className="self-start ml-7 text-lg text-center text-neutral-800 max-md:ml-2.5">
-                            Tom, 24 years old
-                          </div>
+                          
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
                 <div
-                  className="ml-5 w-[54%] max-md:ml-0 max-md:w-full"
+                  className="ml-12 w-[54%] max-md:ml-0 max-md:w-full"
                   data-name="column"
                 >
-                  <div className="flex flex-col grow py-10 pr-4 pl-11 mt-1.5 w-full text-2xl bg-white rounded-[30px] shadow-[0px_5px_4px_rgba(0,0,0,0.25)] text-zinc-800 max-md:pl-5 max-md:mt-10 max-md:max-w-full">
+                  <div className="flex flex-col grow py-10 pr-4 pl-11 mt-4 w-full text-2xl bg-white rounded-[30px] shadow-[0px_5px_4px_rgba(0,0,0,0.25)] text-zinc-800 max-md:pl-5 max-md:mt-10 max-md:max-w-full">
                     <div className="self-center font-bold text-center">
                       Completely changed the game
                     </div>
@@ -73,29 +70,31 @@ const ClientTestimonials = () => {
                       pretium in tincidunt. Suspendisse sed magna eget nibh in
                       turpis. Consequat duis diam lacus arcu. urpis{" "}
                     </div>
-                    <div className="self-start mt-4 text-center max-md:ml-2">
-                      -
+                    <div className="self-start mt-4 text-lg max-md:ml-2">
+                      - Hannah, 29 years old
                     </div>
-                    <div className="self-start ml-9 text-lg text-center text-neutral-800 max-md:ml-2.5">
-                      Hannah, 29 years old
-                    </div>
+                    
                   </div>
                 </div>
               </div>
             </div>
-            <div className="flex flex-wrap gap-5 justify-between items-start mt-5 mr-14 max-w-full w-[513px] max-md:mr-2.5">
-              <img
-                loading="lazy"
-                src="https://cdn.builder.io/api/v1/image/assets/TEMP/82567deb3ce1be1644176b9498f2c34e2a8d54662fc35a77eed8c072ea100d6c?placeholderIfAbsent=true"
-                className="object-contain shrink-0 aspect-square w-[50px]"
-                alt="Testimonial icon 1"
-              />
-              <img
-                loading="lazy"
-                src="https://cdn.builder.io/api/v1/image/assets/TEMP/4e3bb7beda75f84e1047cddcd10305587d4ee1eb4913c076fe8420ebbe79947a?placeholderIfAbsent=true"
-                className="object-contain shrink-0 aspect-square w-[50px]"
-                alt="Testimonial icon 2"
-              />
+            <div className="flex flex-wrap gap-5 justify-between items-center mx-auto mt-0 mr-6 max-w-full w-[513px] max-md:mr-2.5">
+              {/* Left Arrow */}
+              <button className="text-white text-3xl">
+                &lt;
+              </button>
+
+              {/* Dots */}
+              <div className="flex gap-2 justify-center items-center">
+                <div className="w-3 h-3 rounded-full bg-white"></div>
+                <div className="w-3 h-3 rounded-full bg-white"></div>
+                <div className="w-3 h-3 rounded-full bg-white"></div>
+              </div>
+
+              {/* Right Arrow */}
+              <button className="text-white text-3xl">
+                &gt;
+              </button>
             </div>
           </div>
         </div>
@@ -119,10 +118,10 @@ const ClientTestimonials = () => {
                     Suspendisse sed magna eget nibh in turpis. Consequat duis
                     diam lacus arcu
                   </div>
-                  <div className="self-start mt-3 text-2xl text-center">-</div>
-                  <div className="self-start ml-5 text-lg text-center text-neutral-800 max-md:ml-2.5">
-                    Tom, 24 years old
+                  <div className="self-start mt-3 text-lg text-center">
+                    - Tom, 24 years old
                   </div>
+                  
                 </div>
               </div>
               <div
@@ -132,7 +131,7 @@ const ClientTestimonials = () => {
                 <img
                   loading="lazy"
                   srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/0b1a156b25b52a31d1807664004bbb45bea31aedd7c2f0b84ed1698d7e23dc08?placeholderIfAbsent=true&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/0b1a156b25b52a31d1807664004bbb45bea31aedd7c2f0b84ed1698d7e23dc08?placeholderIfAbsent=true&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/0b1a156b25b52a31d1807664004bbb45bea31aedd7c2f0b84ed1698d7e23dc08?placeholderIfAbsent=true&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/0b1a156b25b52a31d1807664004bbb45bea31aedd7c2f0b84ed1698d7e23dc08?placeholderIfAbsent=true&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/0b1a156b25b52a31d1807664004bbb45bea31aedd7c2f0b84ed1698d7e23dc08?placeholderIfAbsent=true&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/0b1a156b25b52a31d1807664004bbb45bea31aedd7c2f0b84ed1698d7e23dc08?placeholderIfAbsent=true&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/0b1a156b25b52a31d1807664004bbb45bea31aedd7c2f0b84ed1698d7e23dc08?placeholderIfAbsent=true&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/0b1a156b25b52a31d1807664004bbb45bea31aedd7c2f0b84ed1698d7e23dc08?placeholderIfAbsent=true"
-                  className="object-contain grow shrink-0 mt-36 max-w-full aspect-[1.06] w-[200px] max-md:mt-10"
+                  className="object-contain grow shrink-0 mt-36 max-w-full aspect-[1.06] w-[300px] h-auto"
                   alt="Client testimonial"
                 />
               </div>
@@ -145,3 +144,4 @@ const ClientTestimonials = () => {
 };
 
 export default ClientTestimonials;
+
