507149eb9772e978011562b99d7f2ba7acede6d6
Tina Ye <tye2868@gmail.com>
2025-02-05 23:55:51 -0800

    Fixed import

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -15,7 +15,7 @@ const NavBar = () => {
       const scrollHandler = () => {
         window.pageYOffset > 10 ? setTop(false) : setTop(true)
       };
-      window.addEventListener('scroll', scrollHandler);
+      window.addEventListener('scroll', scrollHandler);git 
       return () => window.removeEventListener('scroll', scrollHandler);
     }, [top]);
 
