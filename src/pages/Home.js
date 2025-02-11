886f59410e32f8437de6bce488b5185670307e2a
Sodbayar Ganbat <sodbayarg@berkeley.edu>
2025-02-10 20:32:32 -0800

    updated home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import Navbar from '../components/Navbar/NavBar';
-import Clients from '../components/Clients';
 import PersonalityQuiz from "../components/PersonalityQuiz";
 import ClientTestimonials from "../components/ClientTestimonials";
-import Cta from '../components/Cta';
 import Footer from '../components/Footer';
 import Hero from '../components/Hero';
-import Intro from '../components/Intro';
-import Portfolio from '../components/Portfolio';
-import Services from '../components/Services';
 import FAQSection from '../components/FAQ/FAQSection';
 import {HowItWorks} from '../components/CircuitHowItWorks';
 import {AboutSection} from '../components/About'
@@ -27,11 +22,6 @@ const Home = () => {
             <PersonalityQuiz />
             <RestaurantPartnerships/>
             <AboutSection />
-            <Intro />
-            <Services />
-            <Portfolio />
-            <Clients />
-            <Cta/>
             <FAQSection/>
             <Footer />
         </>
