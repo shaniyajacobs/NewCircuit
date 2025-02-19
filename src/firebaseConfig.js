7356f5c7c7477e02c180bdc00772d50d18069bde
Sodbayar Ganbat <sodbayarg@berkeley.edu>
2025-02-18 18:17:55 -0800

    more firebase auth

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,7 +18,10 @@ const firebaseConfig = {
   appId: "1:997074025294:web:3a3faac9dab0155eeb7c1f",
   measurementId: "G-Q9G5S9JKJG"
 };
+const db = getFirestore(app);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+export { auth, db};
