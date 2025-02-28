7e69235d6099f0a3b25a3c64ee02699b5df04ce8
Tina Ye <tye2868@gmail.com>
2025-02-27 19:40:45 -0800

    Fixed quiz navigation

--- src/components/PersonalityQuiz.js
+++ src/components/PersonalityQuiz.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const PersonalityQuiz = () => {
+  const navigate = useNavigate(); 
   return (
     <div
       className="flex flex-col items-center px-20 pt-8 pb-14 text-4xl text-center text-blue-700 bg-zinc-100 max-md:px-5"
@@ -15,12 +16,12 @@ const PersonalityQuiz = () => {
           We match you to your ideal partner based <br />
           on our highly comprehensive personality indicator.
         </div>
-        <Link
-          to="/personalityquizpage/1" // Navigates to the first question of the quiz
+        <button
+          onClick={() => navigate('/quiz-start')}
           className="font-poppins self-center px-5 py-5 mt-6 ml-3.5 max-w-full font-semibold bg-lime-300 rounded-[30px] shadow-[0px_5px_4px_rgba(0,0,0,0.25)] w-[587px] max-md:pr-5 max-md:max-w-full text-center cursor-pointer hover:bg-lime-400 transition"
         >
           Take the Personality Indicator
-        </Link>
+        </button>
       </div>
     </div>
   );
