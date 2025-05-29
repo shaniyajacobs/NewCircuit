424fd394e735b7dd6cd036fda1b060e4c2a13e2f
Daniel Gil <danieltgil05@berkeley.edu>
2025-05-28 21:45:39 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ import EnterpriseProfile from './pages/EnterpriseProfile';
 import EnterpriseUserProfile from './pages/EnterpriseUserProfile';
 import EnterpriseVerifyEmail from './pages/EnterpriseVerifyEmail';
 import EnterpriseDash from './pages/EnterpriseDash';
+import AdminDashboard from './pages/AdminDashboard';
 
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { ProfileProvider } from './contexts/ProfileContext';
@@ -138,6 +139,7 @@ function App() {
             <Route path="/enterprise-user-profile" element={<EnterpriseUserProfile />} />
             <Route path="/enterprise-verify-email" element={<EnterpriseVerifyEmail />} />
             <Route path="/enterprise-dash/*" element={<EnterpriseDash />} />
+            <Route path="/admin-dashboard/*" element={<AdminDashboard />} />
           </Routes>
         </ScrollToTop>
       </Router>
