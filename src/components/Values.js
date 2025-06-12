085946dd4f61055cddbb19f3bd75de9a28e9414b
Mike Guerrero <mikxvii@berkeley.edu>
2025-06-12 11:07:29 -0700

    basically done with the video cleanup

--- src/components/Values.js
+++ src/components/Values.js
@@ -3,23 +3,21 @@ import React from 'react';
 export function Values() {
   return (
     <div 
-        className="max-w-[1280px] mx-auto flex flex-col sm:flex-row justify-center items-center gap-[50px] w-full px-[6vw] py-[100px] bg-[#FAFFE7] min-h-screen"
+        className="max-w-[1280px] mx-auto flex flex-col md:flex-row justify-center items-center gap-[18px] sm:gap-[24px] md:gap-[32px] lg:gap-[50px] w-full px-[6vw] py-[100px] bg-[#FAFFE7] min-h-screen"
     >
       {/* Video Rectangle */}
       <div
         style={{
           display: 'flex',
-          padding: 'var(--Margins-S, 50px)',
           flexDirection: 'column',
           justifyContent: 'flex-end',
           alignItems: 'flex-start',
           flex: '0.8 0 0',
-          alignSelf: 'stretch',
-          borderRadius: 'var(--Radius-M, 16px)',
+          alignSelf: 'center',
           background: 'linear-gradient(180deg, rgba(0, 0, 0, 0.00) 0%, rgba(0, 0, 0, 0.75) 100%), url(<path-to-image>) lightgray 50% / cover no-repeat',
           backgroundBlendMode: 'normal, soft-light',
         }}
-        className="flex min-h-[400px] sm:min-h-[580px] p-[24px] sm:p-[50px] flex-col justify-end items-start flex-[0.8] rounded-[16px] relative overflow-hidden"
+        className="flex w-[358px] min-h-[350px] sm:w-[696px] sm:min-h-[450px] md:w-[592px] md:min-h-[690px] lg:w-[645px] lg:min-h-[923px] p-[16px] sm:p-[24px] md:p-[32px] lg:p-[50px] flex-col justify-end items-start rounded-[8px] sm:rounded-[10px] md:rounded-[12px] lg:rounded-[16px] relative overflow-hidden"
       >
         {/* Video Background */}
         <video
@@ -89,7 +87,7 @@ export function Values() {
               fontWeight: 520,
               lineHeight: '120%',
             }}
-            className="w-fit max-w-[500px] sm:max-w-[600px] md:max-w-[700px] text-[28px] sm:text-[36px] md:text-[50px] text-left max-w-[10000px] md:max-w-[1000px]"
+            className="w-fit max-w-[500px] sm:max-w-[600px] md:max-w-[700px] text-[32px] sm:text-[32px] md:text-[48px] lg:text-[64px] text-left max-w-[10000px] md:max-w-[1000px]"
           >
             Behind the team trying to spark your flame
           </h1>
@@ -97,10 +95,10 @@ export function Values() {
       </div>
 
       {/* Value Boxes Container */}
-      <div className="flex flex-col gap-[30px] flex-1  max-h-[800px]">
+      <div className="flex flex-col gap-[18px] sm:gap-[24px] md:gap-[32px] lg:gap-[50px] flex-1  max-h-[800px]">
         {/* Value 01 */}
         <div
-            className="flex p-[20px] sm:p-[30px] flex-col items-start gap-[20px] sm:gap-[30px] rounded-[16px] bg-[radial-gradient(50%_50%_at_50%_50%,_#E2FF65_0%,_#D2FFD7_100%)]"
+            className="flex w-[358px] h-[137px] sm:w-[696px] sm:h-[135px] md:w-[592px] md:h-[170px] lg:w-[645px] lg:h-[225px] p-[20px] sm:p-[30px] flex-col items-start gap-[16px] sm:gap-[16px] md:gap-[20px] lg:gap-[24px] rounded-[8px] sm:rounded-[10px] md:rounded-[12px] lg:rounded-[16px] bg-[radial-gradient(50%_50%_at_50%_50%,_#E2FF65_0%,_#D2FFD7_100%)]"
         >
             <h2 
                 style={{
@@ -108,11 +106,11 @@ export function Values() {
                     fontWeight: 520,
                     lineHeight: '120%',
                 }}
-                className="text-[28px] sm:text-[32px] md:text-[24px] lg:text-[36px] leading-[110%] text-[#211F20] break-words"
+                className="text-[32px] sm:text-[32px] md:text-[40px] lg:text-[48px] leading-[110%] text-[#211F20] break-words"
             >
                 → Value 01
             </h2>
-            <p className="text-[12px] sm:text-[14px] md:text-[11px] lg:text-[14px] font-poppins font-normal text-[#211F20] break-words">
+            <p className="text-[12px] sm:text-[12px] md:text-[14px] lg:text-[16px] font-poppins font-normal text-[#211F20] break-words">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse mattis metus neque, ac hendrerit risus pharetra ac.
             </p>
         </div>
@@ -120,7 +118,7 @@ export function Values() {
 
         {/* Value 02 */}
         <div
-            className="flex p-[20px] sm:p-[30px] flex-col items-start gap-[20px] sm:gap-[30px] rounded-[16px] bg-[radial-gradient(50%_50%_at_50%_50%,_#8EFF7A_0%,_#D7FFF8_100%)]"
+            className="flex w-[358px] h-[137px] sm:w-[696px] sm:h-[135px] md:w-[592px] md:h-[170px] lg:w-[645px] lg:h-[225px] p-[20px] sm:p-[30px] flex-col items-start gap-[16px] sm:gap-[16px] md:gap-[20px] lg:gap-[24px] rounded-[8px] sm:rounded-[10px] md:rounded-[12px] lg:rounded-[16px] bg-[radial-gradient(50%_50%_at_50%_50%,_#8EFF7A_0%,_#D7FFF8_100%)]"
         >
             <h2 
                 style={{
@@ -128,11 +126,11 @@ export function Values() {
                     fontWeight: 520,
                     lineHeight: '120%',
                 }}
-                className="text-[28px] sm:text-[32px] md:text-[24px] lg:text-[36px] leading-[110%] text-[#211F20] break-words"
+                className="text-[32px] sm:text-[32px] md:text-[40px] lg:text-[48px] leading-[110%] text-[#211F20] break-words"
             >
                 → Value 02
             </h2>
-            <p className="text-[12px] sm:text-[14px] md:text-[11px] lg:text-[14px] font-poppins font-normal text-[#211F20] break-words">
+            <p className="text-[12px] sm:text-[12px] md:text-[14px] lg:text-[16px] font-poppins font-normal text-[#211F20] break-words">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse mattis metus neque, ac hendrerit risus pharetra ac.
             </p>
         </div>
@@ -140,20 +138,20 @@ export function Values() {
 
         {/* Value 03 */}
         <div
-            className="flex flex-1 min-h-[300px] p-[20px] sm:p-[30px] flex-col items-start gap-[20px] sm:gap-[30px] rounded-[16px] bg-[radial-gradient(50%_50%_at_50%_50%,_#79FFC7_0%,_#FFF9A1_100%)]"
+            className="flex w-[358px] h-[195px] sm:w-[696px] sm:h-[219px] md:w-[592px] md:h-[286px] lg:w-[645px] lg:h-[373px] p-[20px] sm:p-[30px] flex-col items-start gap-[16px] sm:gap-[16px] md:gap-[20px] lg:gap-[24px] rounded-[8px] sm:rounded-[10px] md:rounded-[12px] lg:rounded-[16px] bg-[radial-gradient(50%_50%_at_50%_50%,_#79FFC7_0%,_#FFF9A1_100%)]"
         >
-            <div className="flex flex-col items-start gap-[20px] sm:gap-[30px]">
+            <div className="flex flex-col items-start gap-[16px] sm:gap-[16px] md:gap-[20px] lg:gap-[24px]">
                 <h2 
                     style={{
                         fontFamily: '"Bricolage Grotesque", sans-serif',
                         fontWeight: 520,
                         lineHeight: '120%',
                     }}
-                    className="text-[28px] sm:text-[32px] md:text-[24px] lg:text-[36px] leading-[110%] text-[#211F20] break-words"
+                    className="text-[32px] sm:text-[32px] md:text-[40px] lg:text-[48px] leading-[110%] text-[#211F20] break-words"
                 >
                     → Value 03
                 </h2>
-                <p className="text-[12px] sm:text-[14px] md:text-[11px] lg:text-[14px] font-poppins font-normal text-[#211F20] break-words">
+                <p className="text-[12px] sm:text-[12px] md:text-[14px] lg:text-[16px] font-poppins font-normal text-[#211F20] break-words">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse mattis metus neque, ac hendrerit risus pharetra ac.
                 </p>
             </div>
